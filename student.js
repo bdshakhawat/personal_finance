af91fdff9915d7ce49ec5a9e98f920fb25bf52c5
bdshakhawat <mdshakwat.hossain@gmail.com>
2022-11-21 16:23:20 +0600

    other comit

---
+++ student.js
@@ -0,0 +1,73 @@
+function personalFinance(students){
+    for (i=0; i<students.length; i++) {
+        
+        // console.log(students[i]["student"])
+        let studentInfo = students[i]["student"];
+        
+        // // Creating Table Header
+        // let h=document.getElementById('Table_data');
+        // let Batch= (students[i]['batchName']);
+        // h.innerHTML=Batch;
+
+        // // console.log(studentInfo);
+        // //   Creating Data Table
+        // var myTable = document.createElement("table");
+        // for (j=0; j<studentInfo.length; j++) {
+        //     let allStudents= studentInfo[j];
+        //     console.log(allStudents["class"]);
+            
+        //     const tr = myTable.insertRow();
+        //     const td = tr.insertCell();
+        //     td.appendChild(document.createTextNode(`${allStudents["fullName"]}`));
+        //     td.appendChild(document.createTextNode(`${allStudents["class"]}`));
+        //     td.appendChild(document.createTextNode(`${allStudents["startDate"]}`));
+        // }
+        // let tableContainer = document.getElementById("manage_section");
+        // tableContainer.appendChild(myTable);
+
+    }
+}
+
+  
+
+    
+    
+    
+      
+
+
+    
+    
+  
+ 
+    
+    
+    
+
+    
+   
+    
+    
+   
+    
+
+
+   
+  
+
+
+   
+
+
+
+
+
+
+  
+ 
+
+
+
+
+
+
